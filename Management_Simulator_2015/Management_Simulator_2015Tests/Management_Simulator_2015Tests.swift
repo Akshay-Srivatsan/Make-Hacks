e5be326ef0a8f16e7d9a1504e0b7fb4392b08e5f
Will Borie <will.borie@gmail.com>
2015-04-26 09:40:50 -0700

    Created Management Sim

---
+++ Management_Simulator_2015/Management_Simulator_2015Tests/Management_Simulator_2015Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Management_Simulator_2015Tests.swift
+//  Management_Simulator_2015Tests
+//
+//  Created by William Borie on 4/26/15.
+//  Copyright (c) 2015 William Borie. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Management_Simulator_2015Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
